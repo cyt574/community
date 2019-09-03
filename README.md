15c5815fad11bcdcd9c67b94e5a00f06986cacb0
cyt574 <qq418894140@gmail.com>
2019-09-03 17:49:58 +0800

    add sql ddl

--- README.md
+++ README.md
@@ -1,8 +1,34 @@
 ##以辰社区
 ##资料
 [Spring文档](https://spring.io/guides)
+
 [ES](https://elasticsearch.cn/)
+
 [Bootstrap](https://v3.bootcss.com/)
+
 [Github OAuth](https://developer.github.com/v3/guides/managing-deploy-keys/#deploy-keys)
+
+[okhttp](https://square.github.io/okhttp/)
+
+[mybatis](http://www.mybatis.org/spring-boot-starter/mybatis-spring-boot-autoconfigure/)
+
 ##工具
+
 [Git](https://git-scm.comg)
+
+##脚本
+```sql
+create table USER
+(
+	ID INTEGER auto_increment,
+	ACCOUNT_ID VARCHAR(100),
+	NAME VARCHAR(50),
+	TOKEN CHAR(36),
+	GMT_CREATE BIGINT,
+	GMT_MODIFIED BIGINT,
+	constraint USER_PK
+		primary key (ID)
+);
+```
+
+ 
--- src/main/java/com/yichen/community/controller/AuthorizeController.java
+++ src/main/java/com/yichen/community/controller/AuthorizeController.java
@@ -2,12 +2,17 @@ package com.yichen.community.controller;
 
 import com.yichen.community.dto.AccessTokenDTO;
 import com.yichen.community.dto.GithubUser;
+import com.yichen.community.mapper.UserMapper;
+import com.yichen.community.model.User;
 import com.yichen.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
@@ -24,8 +29,11 @@ public class AuthorizeController {
     @Value("${github.redirect.uri}")
     private String redirectUri;
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/callback")
-    public String callback(@RequestParam(name = "code") String code, @RequestParam(name = "state") String state) {
+    public String callback(@RequestParam(name = "code") String code, @RequestParam(name = "state") String state, HttpServletRequest request) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
         accessTokenDTO.setClient_id(clientId);
@@ -33,8 +41,19 @@ public class AuthorizeController {
         accessTokenDTO.setState(state);
         accessTokenDTO.setRedirect_uri(redirectUri);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        if(githubUser !=null) {
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setName(githubUser.getName());
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            request.getSession().setAttribute("user", user);
+            return "redirect:/";
+        }else {
+            return "redirect:/";
+        }
     }
 }

198a1f52cf41f96a7f275e71e6c7373e602d79fb
cyt574 <qq418894140@gmail.com>
2019-09-03 18:22:26 +0800

    add cookie check

--- src/main/java/com/yichen/community/controller/IndexController.java
+++ src/main/java/com/yichen/community/controller/IndexController.java
@@ -1,13 +1,33 @@
 package com.yichen.community.controller;
 
+import com.yichen.community.mapper.UserMapper;
+import com.yichen.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class IndexController {
 
+    @Autowired
+    UserMapper userMapper;
+
     @GetMapping("/")
-    public String index(){
+    public String index(HttpServletRequest httpServletRequest){
+        Cookie[] cookies = httpServletRequest.getCookies();
+        for (Cookie cookie : cookies) {
+            if(cookie.getName().equals("token")) {
+                String token = cookie.getValue();
+                User user = userMapper.findByToken(token);
+                if(user != null) {
+                    httpServletRequest.getSession().setAttribute("user", user);
+                }
+                break;
+            }
+        }
         return "index";
     }
 }

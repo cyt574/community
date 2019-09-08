a60464e1bfff4c8d6852b5782cb490036330e8d4
cyt574 <418894140@qq.com>
2019-09-09 04:51:24 +0800

    fix some bugs

--- src/main/resources/db/migration/V7__Change_id_column_type.sql
+++ src/main/resources/db/migration/V7__Change_id_column_type.sql
@@ -1,2 +1,2 @@
-alter table QUESTION alter column ID BIGINT default not null auto_increment;
-alter table USER alter column ID BIGINT default not null auto_increment;
+alter table question modify id bigint default not null auto_increment;
+alter table user modify id bigint default not null auto_increment;
